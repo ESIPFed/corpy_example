96905283c7c3aed4adc8d90e0fa9ef80979e0f9c
Carlos Rueda <carueda@mbari.org>
2019-01-31 14:29:10 -0800

    adjustments after latest updates of corpy
    corpy updated by Lewis during slack session

--- demo1.py
+++ demo1.py
@@ -5,31 +5,31 @@
 
 if __name__ == "__main__":
     from pprint import pprint
+    import swagger_client
 
-    from swagger_client import ApiClient
-    from swagger_client import OntologyApi
-    from swagger_client import OrganizationApi
-    from swagger_client import TermApi
+    configuration = swagger_client.Configuration()
+    # Note: no authenticated operations in this simple demo.
+    api_client = swagger_client.ApiClient(configuration)
 
-    # explicitly indicate the API endpoint:
-    host = 'http://cor.esipfed.org/ont/api/v0'
-    print("Using endpoint " + host)
-
-    # With not authentication:
-    api_client = ApiClient(host=host)
-
-    ont_api = OntologyApi(api_client)
-    org_api = OrganizationApi(api_client)
-    term_api = TermApi(api_client)
-
-    print('\n' + '=' * 20)
-    print('Ontologies:')
+    # ontologies
+    ont_api = swagger_client.OntologyApi(api_client)
+    print('\n\nOntologies' + '=' * 50 + '\n')
     pprint(ont_api.ont_get())
 
-    print('\n' + '=' * 20)
+    iri = 'http://sweetontology.net/realmBiolBiome'
+    format = 'ttl'
+    print('\n\nOntology by iri=%s format=%s' % (iri, format) + '=' * 50 + '\n')
+    response = ont_api.ont_get(iri=iri, format=format)
+    pprint(response)
+
+    # organizations
+    org_api = swagger_client.OrganizationApi(api_client)
+    print('\n\nOrganizations' + '=' * 50 + '\n')
     print('Organizations:')
     pprint(org_api.org_get())
 
-    print('\n' + '=' * 20)
-    print('Terms containing "temperature" in SPO:')
-    pprint(term_api.term_get(containing='temperature', _in='spo'))
+    # terms
+    term_api = swagger_client.TermApi(api_client)
+    containing = 'temperature'
+    print('\n\nTerms containing "%s" in SPO' % containing + '=' * 50 + '\n')
+    pprint(term_api.term_get(containing=containing, _in='spo'))
